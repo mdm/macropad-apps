da9d11677d9e3da0f3a8fe1af9944c29c934680b
Marc Dominik Migge <marcmigge@gmx.net>
2023-07-02 20:26:39 +0200

    Scan all keys in input handler and hook up display

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -5,19 +5,7 @@ use embassy_time::{Duration, Timer};
 #[derive(Clone)]
 pub enum InputSource {
     Button,
-    RotaryEncoder,
-    Key1,
-    Key2,
-    Key3,
-    Key4,
-    Key5,
-    Key6,
-    Key7,
-    Key8,
-    Key9,
-    Key10,
-    Key11,
-    Key12,
+    Key(usize),
 }
 
 #[derive(Clone)]
@@ -28,25 +16,27 @@ pub enum InputEvent {
     TurnedCCW,
 }
 
+const NUM_KEYS: usize = 12;
+
 pub struct InputHandler<'a, const SUBS: usize> {
     button_input: Input<'a, AnyPin>,
     button_active: bool,
-    key_inputs: [Input<'a, AnyPin>; 12],
-    key_active: [bool; 12],
+    key_inputs: [Input<'a, AnyPin>; NUM_KEYS],
+    key_active: [bool; NUM_KEYS],
     publisher: Publisher<'a, ThreadModeRawMutex, InputEvent, 4, SUBS, 1>,
 }
 
 impl<'a, const SUBS: usize> InputHandler<'a, SUBS> {
     pub fn new(
         button: AnyPin,
-        keys: [AnyPin; 12],
+        keys: [AnyPin; NUM_KEYS],
         publisher: Publisher<'a, ThreadModeRawMutex, InputEvent, 4, SUBS, 1>,
     ) -> Self {
         let button_input = Input::new(button, Pull::Up);
         let button_active = false;
 
         let key_inputs = keys.map(|key| Input::new(key, Pull::Up));
-        let key_active = [false; 12];
+        let key_active = [false; NUM_KEYS];
 
         InputHandler {
             button_input,
@@ -63,13 +53,31 @@ impl<'a, const SUBS: usize> InputHandler<'a, SUBS> {
             if !self.button_active && self.button_input.is_low() {
                 self.button_active = true;
                 self.publisher
-                    .publish(InputEvent::Pressed(InputSource::Button)).await;
+                    .publish(InputEvent::Pressed(InputSource::Button))
+                    .await;
             }
             if self.button_active && self.button_input.is_high() {
                 self.button_active = false;
                 self.publisher
-                    .publish(InputEvent::Released(InputSource::Button)).await;
+                    .publish(InputEvent::Released(InputSource::Button))
+                    .await;
             }
+
+            for (i, key_input) in self.key_inputs.iter().enumerate() {
+                if !self.key_active[i] && key_input.is_low() {
+                    self.key_active[i] = true;
+                    self.publisher
+                        .publish(InputEvent::Pressed(InputSource::Key(i)))
+                        .await;
+                }
+                if self.key_active[i] && key_input.is_high() {
+                    self.key_active[i] = false;
+                    self.publisher
+                        .publish(InputEvent::Released(InputSource::Key(i)))
+                        .await;
+                }
+            }
+
             Timer::after(interval).await;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
-use core::convert::Infallible;
 
-// use adafruit_macropad::{
-//     entry,
-//     hal::{
-//         clocks::{init_clocks_and_plls, Clock},
-//         gpio::FunctionSpi,
-//         pac,
-//         pio::PIOExt,
-//         rosc::RingOscillator,
-//         watchdog::Watchdog,
-//         Sio, Spi, Timer,
-//     },
-//     Pins, XOSC_CRYSTAL_FREQ,
-// };
+use core::cell::RefCell;
+
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_rp::{
-    gpio::{Level, Output, AnyPin},
+    clocks::RoscRng,
+    gpio::{AnyPin, Level, Output},
+    pac::{pio::StateMachine, resets::regs::Peripherals},
+    peripherals::PIO0,
+    pio::{Instance, Pio}, spi::{Spi, Blocking, self},
 };
 use embassy_sync::{
-    blocking_mutex::raw::ThreadModeRawMutex,
+    blocking_mutex::{raw::{ThreadModeRawMutex, NoopRawMutex}, Mutex},
     pubsub::{PubSubChannel, Subscriber, WaitResult},
 };
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Timer, Delay};
 use embedded_graphics::{
     image::{Image, ImageRawLE},
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
@@ -32,17 +25,18 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
 };
-use embedded_hal::{digital::v2::InputPin, digital::v2::OutputPin, spi::MODE_0};
+use embedded_hal::{digital::v2::InputPin, digital::v2::OutputPin, spi::MODE_0, timer::CountDown};
 use fugit::RateExtU32;
 use menu::Menu;
-use panic_semihosting as _;
+use panic_halt as _;
 use rand::Rng;
+use rp2040_hal::rosc::{Enabled, RingOscillator};
 use sh1106::{prelude::*, Builder};
 use smart_leds::{
     hsv::{hsv2rgb, Hsv},
-    SmartLedsWrite,
+    SmartLedsWrite, RGB8,
 };
-use ws2812_pio::Ws2812;
+use ws2812_pio_embassy::Ws2812;
 
 use input_handler::{InputEvent, InputHandler, InputSource};
 
@@ -52,6 +46,8 @@ mod menu;
 const SUBS: usize = 4;
 static INPUT_CHANNEL: PubSubChannel<ThreadModeRawMutex, InputEvent, 4, SUBS, 1> =
     PubSubChannel::new();
+
+const NEOPIXEL_NUM_LEDS: usize = 12;
 
 #[embassy_executor::task]
 async fn blinker_task(mut led: Output<'static, AnyPin>, interval: Duration) {
@@ -75,6 +71,39 @@ async fn blinker_task(mut led: Output<'static, AnyPin>, interval: Duration) {
                 Timer::after(interval).await;
             }
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn color_fader_task(mut ws2812: Ws2812<'static, PIO0, 0, NEOPIXEL_NUM_LEDS>) {
+    let mut input_subscriber = INPUT_CHANNEL.subscriber().unwrap();
+
+    let mut hues_and_values = [(0, 0); 12];
+
+    loop {
+        if let Some(WaitResult::Message(InputEvent::Pressed(InputSource::Key(key)))) =
+            input_subscriber.try_next_message()
+        {
+            hues_and_values[key] = (RoscRng.gen::<u8>(), 255);
+        }
+
+        let data = hues_and_values.map(|(hue, val)| {
+            let hsv = Hsv { hue, sat: 255, val };
+
+            hsv2rgb(hsv)
+        });
+
+        ws2812.write(&data).await;
+
+        hues_and_values = hues_and_values.map(|mut hv| {
+            if hv.1 > 0 {
+                hv.1 -= 5;
+            }
+
+            hv
+        });
+
+        Timer::after(Duration::from_millis(10)).await;
     }
 }
 
@@ -106,11 +135,52 @@ async fn main(spawner: Spawner) {
     let input_handler = InputHandler::new(button, keys, input_publisher);
     spawner.spawn(input_handler_task(input_handler)).unwrap();
 
-    let led: AnyPin = peripherals.PIN_13.into(); 
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(peripherals.PIO0);
+    let ws2812 = Ws2812::new(&mut common, sm0, peripherals.DMA_CH0, peripherals.PIN_19);
+    spawner.spawn(color_fader_task(ws2812)).unwrap();
+
+    let led: AnyPin = peripherals.PIN_13.into();
     let led = Output::new(led, Level::Low);
     spawner
         .spawn(blinker_task(led, Duration::from_millis(300)))
         .unwrap();
 
-    // let mut hues_and_values = [(0, 0); 12];
+    let sclk = peripherals.PIN_26;
+    let mosi = peripherals.PIN_27;
+    let miso = peripherals.PIN_28;
+    let mut display_config = spi::Config::default();
+    display_config.frequency = 10_000_000;
+    let spi: Spi<'_, _, Blocking> = Spi::new_blocking(peripherals.SPI1, sclk, mosi, miso, display_config);
+
+    let oled_cs = Output::new(peripherals.PIN_22, Level::Low);
+    let mut oled_reset = Output::new(peripherals.PIN_23, Level::Low);
+    let oled_dc = Output::new(peripherals.PIN_24, Level::Low);
+
+    let mut display: GraphicsMode<_> = Builder::new().connect_spi(spi, oled_dc, oled_cs).into();
+
+    display.reset(&mut oled_reset, &mut Delay).unwrap();
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    let mut menu = Menu::new(
+        &[
+            "Hello Rust!",
+            "Hello world!",
+            "Hello Marc!",
+            "Test Item 4",
+            "Test Item 5",
+        ],
+        display.size().height,
+        &FONT_6X10,
+        BinaryColor::Off,
+        BinaryColor::On,
+    );
+    menu.draw(&mut display).unwrap();
+    display.flush().unwrap();
+
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+    }
 }
