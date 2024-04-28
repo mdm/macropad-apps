f49cb75a4473c2f6287d706b1b0920b515c9fcdc
Marc Dominik Migge <marcmigge@gmx.net>
2024-04-28 13:24:49 +0200

    Add rest of the datetime setting menus

--- src/rtc.rs
+++ src/rtc.rs
@@ -1,4 +1,4 @@
-use core::fmt::Write;
+use core::{fmt::Write, num, ops::RangeInclusive};
 
 use embassy_rp::i2c::{I2c, Instance, Mode};
 use heapless::{String, Vec};
@@ -33,21 +33,23 @@ impl<'d, T: Instance, M: Mode> Rtc<'d, T, M> {
     {
         let current_datetime = self.get_datetime();
 
-        let mut years: Vec<String<4>, 11> = Vec::new();
-        for year in (current_datetime.year - MENU_RANGE as u16)
-            ..=(current_datetime.year + MENU_RANGE as u16)
-        {
-            let mut year_str = String::new();
-            let _ = write!(year_str, "{year}");
-            let _ = years.push(year_str);
-        }
+        // set the year
+        let Some(year_choice) = self
+            .choose_from_range(
+                (current_datetime.year as usize - MENU_RANGE)
+                    ..=(current_datetime.year as usize + MENU_RANGE),
+                MENU_RANGE,
+                display,
+                display_height,
+            )
+            .await
+        else {
+            return;
+        };
 
-        let years = years.iter().map(|s| s.as_str()).collect::<Vec<_, 11>>();
+        let year = current_datetime.year as usize - MENU_RANGE + year_choice;
 
-        let mut year_menu = MenuManager::new(years.as_slice(), display_height);
-        year_menu.select_item(MENU_RANGE);
-        let year_choice = year_menu.choose(display).await;
-
+        // set the month
         let months = [
             "January",
             "February",
@@ -65,7 +67,70 @@ impl<'d, T: Instance, M: Mode> Rtc<'d, T, M> {
 
         let mut month_menu = MenuManager::new(&months, display_height);
         month_menu.select_item(current_datetime.month as usize - 1);
-        let month_choice = month_menu.choose(display).await;
+        let Some(month_choice) = month_menu.choose(display).await else {
+            return;
+        };
+
+        let month = month_choice + 1;
+
+        // set the day
+        let num_days = match month {
+            2 => 28,
+            4 | 6 | 9 | 11 => 30,
+            _ => 31,
+        };
+        let Some(day_choice) = self
+            .choose_from_range(
+                1..=num_days,
+                current_datetime.day as usize - 1,
+                display,
+                display_height,
+            )
+            .await
+        else {
+            return;
+        };
+
+        let day = day_choice + 1;
+
+        // set the hours
+        let Some(hours) = self
+            .choose_from_range(
+                0..=23,
+                current_datetime.hours as usize,
+                display,
+                display_height,
+            )
+            .await
+        else {
+            return;
+        };
+
+        // set the minutes
+        let Some(minutes) = self
+            .choose_from_range(
+                0..=59,
+                current_datetime.minutes as usize,
+                display,
+                display_height,
+            )
+            .await
+        else {
+            return;
+        };
+
+        // set the seconds
+        let Some(seconds) = self
+            .choose_from_range(
+                0..=59,
+                current_datetime.seconds as usize,
+                display,
+                display_height,
+            )
+            .await
+        else {
+            return;
+        };
     }
 
     pub fn set_datetime(&mut self, time: &DateTime) {
@@ -89,5 +154,30 @@ impl<'d, T: Instance, M: Mode> Rtc<'d, T, M> {
             minutes,
             seconds,
         }
+    }
+
+    async fn choose_from_range<DI>(
+        &mut self,
+        range: RangeInclusive<usize>,
+        default: usize,
+        display: &mut GraphicsMode<DI>,
+        display_height: u32,
+    ) -> Option<usize>
+    where
+        DI: DisplayInterface,
+    {
+        let mut items: Vec<String<4>, 64> = Vec::new();
+        for item in range {
+            let mut item_str = String::new();
+            let _ = write!(item_str, "{item}");
+            let _ = items.push(item_str);
+        }
+
+        let items = items.iter().map(|s| s.as_str()).collect::<Vec<_, 64>>();
+
+        // TODO: use custom logic instead of MenuManager
+        let mut menu = MenuManager::new(items.as_slice(), display_height);
+        menu.select_item(default);
+        menu.choose(display).await
     }
 }
